b1fcc3acdfa492d77d7553c02eef775400dee031
Jon Simons <simonsj@ccs.neu.edu>
2011-05-22 23:31:33 -0700

    Rename and fixup examples/realistic.vala -> .lua

--- examples/realistic.lua
+++ examples/realistic.lua
@@ -2,16 +2,16 @@ local my_class = class
 local my_ship = ships[my_class]
 
 while true do
-	thrust_main(my_ship.max_acc)
+	thrust_main(my_ship.max_main_acc)
 	sleep(32)
-	thrust_main(-my_ship.max_acc)
+	thrust_main(-my_ship.max_main_acc)
 	sleep(32)
 	thrust_main(0)
 	yield()
 
-	thrust_lateral(my_ship.max_acc)
+	thrust_lateral(my_ship.max_main_acc)
 	sleep(32)
-	thrust_lateral(-my_ship.max_acc)
+	thrust_lateral(-my_ship.max_main_acc)
 	sleep(32)
 	thrust_lateral(0)
 	yield()
